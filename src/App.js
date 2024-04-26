5fd2a679142f8fda6bf7332d484baa2771d7c086
Farhan Sadeek <farhansadeek019@gmail.com>
2024-04-26 08:32:39 -0400

    finished leftsidebar

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@ import "./App.css";
 import gptLogo from "./assets/chatgpt.svg";
 import addBtn from "./assets/add-30.png";
 import msgIcon from "./assets/message.svg";
+import home from "./assets/home.svg";
+import saved from "./assets/bookmark.svg";
+import rocket from "./assets/rocket.svg";
+
 
 function App() {
     return (
         <div className="App">
             <div className="sideBar">
-                <div className="upperside">
+                <div className="upperSide">
                     <div className="upperSideTop">
                         <img src={gptLogo} alt="Logo" className="logo" />
                         <span className="brand">ChatGPT</span>
-                        <button className="midBtn">
-                            <img
-                                src={addBtn}
-                                alt="New Chat"
-                                className="addBtn"
-                            />
-                            New Chat
-                        </button>
                     </div>
+                    <button className="midBtn">
+                        <img src={addBtn} alt="New Chat" className="addBtn" />
+                        New Chat
+                    </button>
+
                     <div className="upperSideBottom">
                         <button className="query">
                             <img src={msgIcon} alt="Message Icon" />
@@ -31,17 +32,21 @@ function App() {
                         </button>
                     </div>
                 </div>
-                <div className="lowerside">
+                <div className="lowerSide">
                     <div className="listItems">
-                        <img src="" alt="" className="listitemsImg" />
+                        <img src={home} alt="Home" className="listItemsImg" />
                         Home
                     </div>
                     <div className="listItems">
-                        <img src="" alt="" className="listitemsImg" />
+                        <img src={saved} alt="Saved" className="listItemsImg" />
                         Save
                     </div>
                     <div className="listItems">
-                        <img src="" alt="" className="listitemsImg" />
+                        <img
+                            src={rocket}
+                            alt="Upgrade"
+                            className="listItemsImg"
+                        />
                         Upgrade to Pro
                     </div>
                 </div>
