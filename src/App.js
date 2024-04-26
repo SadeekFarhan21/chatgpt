4dde1b835de9755c56d1daa742cefdf356f3b3a2
Farhan Sadeek <farhansadeek019@gmail.com>
2024-04-26 08:06:32 -0400

    creating the left side of the screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import gptLogo from "./assets/chatgpt.svg";
+import addBtn from "./assets/add-30.png";
+import msgIcon from "./assets/message.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="sideBar">
+                <div className="upperside">
+                    <div className="upperSideTop">
+                        <img src={gptLogo} alt="Logo" className="logo" />
+                        <span className="brand">ChatGPT</span>
+                        <button className="midBtn">
+                            <img
+                                src={addBtn}
+                                alt="New Chat"
+                                className="addBtn"
+                            />
+                            New Chat
+                        </button>
+                    </div>
+                    <div className="upperSideBottom">
+                        <button className="query">
+                            <img src={msgIcon} alt="Message Icon" />
+                            What is Programming?
+                        </button>
+                        <button className="query">
+                            <img src={msgIcon} alt="Message Icon" />
+                            How to use an API?
+                        </button>
+                    </div>
+                </div>
+                <div className="lowerside">
+                    <div className="listItems">
+                        <img src="" alt="" className="listitemsImg" />
+                        Home
+                    </div>
+                    <div className="listItems">
+                        <img src="" alt="" className="listitemsImg" />
+                        Save
+                    </div>
+                    <div className="listItems">
+                        <img src="" alt="" className="listitemsImg" />
+                        Upgrade to Pro
+                    </div>
+                </div>
+            </div>
+            <div className="main"></div>
+        </div>
+    );
 }
 
 export default App;
